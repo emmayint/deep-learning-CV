6c0915fdb8ec3201086759159fb867de4b423f54
sushilkplassar <sushilkplassar@gmail.com>
2019-12-25 12:54:15 -0800

    added db Schema and python ML API code

---
+++ dbSchema/table_schema.sql
@@ -0,0 +1,91 @@
+CREATE TABLE `experiment_cropped_images` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `exp_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `exp_img_id` int(11) NOT NULL,
+  `exp_crop_img` varchar(255) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `exp_label_name` varchar(255) NOT NULL,
+  `rect` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=135 DEFAULT CHARSET=latin1;
+
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) DEFAULT NULL,
+  `email` varchar(50) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
+
+
+CREATE TABLE `experiments` (
+  `exp_id` int(11) NOT NULL AUTO_INCREMENT,
+  `users_id` int(11) NOT NULL,
+  `exp_title` varchar(40) NOT NULL DEFAULT '',
+  `exp_birth_date` date NOT NULL,
+  `exp_misc_data` json DEFAULT NULL,
+  PRIMARY KEY (`exp_id`),
+  KEY `users_id` (`users_id`),
+  CONSTRAINT `experiments_ibfk_1` FOREIGN KEY (`users_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=31 DEFAULT CHARSET=utf8;
+
+
+CREATE TABLE `experiment_images` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `exp_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `exp_images` varchar(255) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `update_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `rect` text CHARACTER SET latin1,
+  `channel` varchar(255) DEFAULT '',
+  PRIMARY KEY (`id`),
+  KEY `exp_id` (`exp_id`),
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `experiment_images_ibfk_1` FOREIGN KEY (`exp_id`) REFERENCES `experiments` (`exp_id`),
+  CONSTRAINT `experiment_images_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `experiments` (`users_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=108 DEFAULT CHARSET=utf8;
+
+
+CREATE TABLE `prediction` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `exp_id` int(11) NOT NULL,
+  `exp_img_id` int(11) NOT NULL,
+  `crop_id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `img` varchar(255) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `exp_type` varchar(225) NOT NULL,
+  `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `validateResult` tinyint(4) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=322 DEFAULT CHARSET=utf8;
+
+
+CREATE TABLE `prediction_type` (
+  `id` int(50) NOT NULL AUTO_INCREMENT,
+  `exp_id` int(50) NOT NULL,
+  `exp_img_id` int(50) NOT NULL,
+  `img` varchar(50) NOT NULL,
+  `exp_type` varchar(100) NOT NULL,
+  `created_at` datetime NOT NULL,
+  `update_at` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `user_validate` varchar(45) DEFAULT NULL,
+  `exp_validate` tinyint(4) NOT NULL DEFAULT '0',
+  `pred_percentage` json DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=188 DEFAULT CHARSET=utf8;
+
+
+CREATE TABLE `sessions` (
+  `session_id` varchar(128) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `expires` int(11) unsigned NOT NULL,
+  `data` text CHARACTER SET utf8 COLLATE utf8_bin,
+  PRIMARY KEY (`session_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
