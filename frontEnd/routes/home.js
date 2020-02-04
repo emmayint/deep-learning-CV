060b82e230a976647cb13f268414937811972e9a
Sushil Plassar <sushilkplassar@Sushils-MacBook-Pro.local>
2020-02-03 22:58:29 -0800

    Query added to fetch summary results

--- frontEnd/routes/home.js
+++ frontEnd/routes/home.js
@@ -5,7 +5,7 @@ let router = express.Router();
 // @route   GET /home
 // @desc    Retrieve all the experiment created by the user
 // @access  Private
-router.get('/', function (req, res, next) {
+router.get('/gallary', function (req, res, next) {
     if (req.isAuthenticated()) {
         let user = req.user;
 
@@ -17,6 +17,26 @@ db.query('SELECT experiment_images.exp_id, experiment_images.user_id, DATE_FORMA
         console.log('user authenticated');
         console.log(this.sql);
             res.render('home', {uname: user.user_name, data: results});
+        });
+    } else {
+        res.redirect('/');
+    }
+});
+
+router.get('/', function (req, res, next) {
+    if (req.isAuthenticated()) {
+        let user = req.user;
+        console.log("M HERE--->", user.user_id);
+    db.query('SELECT p.exp_id, DATE_FORMAT(p.created_at,"%m/%d/%Y %T") AS prediction_date, e.exp_title, "VGG-16" as model_name, '
+            + 'COUNT(p.exp_img_id) as total_images, SUM(case when p.exp_type= "CONTROL" then 1 else 0 end) as control_count, '
+            + 'SUM(case when p.exp_type= "MUTANT" then 1 else 0 end) as mutant_count ' 
+            + 'FROM prediction_type p, experiments e '
+            + 'WHERE p.exp_id = e.exp_id AND e.users_id = ' + user.user_id + ' '
+            + 'GROUP BY p.exp_id,p.created_at, e.exp_title ORDER BY p.created_at desc;', function (error, results, fields) {
+            if (error) throw error;
+        console.log('Inside Home/Summary GET Request');
+        console.log(this.sql);
+            res.render('home', {uname: user.user_name, summary_data: results});
         });
     } else {
         res.redirect('/');
